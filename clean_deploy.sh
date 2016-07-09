3e3afb22cece80e0dfdbf2e8db52a4d9e0f4d585
Chris Ward <chriswhward@gmail.com>
2016-07-09 23:05:03 +0300

    Don't need deploy scripts anymore

--- clean_deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# TODO: Rsync and other options may be better.
-
-sh ./clean_build.sh
-cd _site
-
-ssh deployer@46.101.5.26 bash -c "cd /var/www/gm; rm -dfr *"
-scp -r * deployer@46.101.5.26:/var/www/gm
